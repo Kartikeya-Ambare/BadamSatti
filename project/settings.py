9cb47a10a812f97109add9552be9a1ed9457b6c3
Kartikeya-Ambare-new <ambarekd@rknec.edu>
2025-08-08 19:16:49 +0530

    Pushed on the Github

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$^iz%o74fkv21wdopbhwczp6wp9brb2d+6h)d)6ginrguoqu==
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.ngrok-free.app','*','KartikeyaAmbare.pythonanywhere.com']
+ALLOWED_HOSTS = ['.ngrok-free.app','*','KartikeyaAmbare.pythonanywhere.com','.vercel.app']
 
 
 # Application definition
